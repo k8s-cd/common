6b0ab4f1f414ceabf2ebffc050089095d56fb72c
elapse-d <36126914+elapse-d@users.noreply.github.com>
2026-02-07 14:30:11 +0100

    Add script to set up ArgoCD core-like environment
    This script sets up an ArgoCD core-like environment, creating the necessary namespace and deploying required resources.

---
+++ argocd-core-like.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+###############################################################
+#  ARGOCD-CORE-LIKE because argocd core not work by default
+#  https://github.com/argoproj/argo-cd/issues/12903
+###############################################################
+
+#curl -sSL https://raw.githubusercontent.com/k8s-cd/common/main/argocd-core-like.sh | bash
+set -euo pipefail
+use_sudo=true 
+if $use_sudo ; then
+  sudo -i
+fi
+cluster_name="${1:-${KIND_CLUSTER:-kind-nginx}}"
+k="kubectl --kubeconfig $HOME/.kube/$cluster_name"
+ns=argocd-core
+
+$k create ns $ns
+k="$k -n $ns "
+$k create -f https://raw.githubusercontent.com/argoproj/argo-cd/refs/heads/master/manifests/core-install.yaml
+$k wait --for=condition=Available deployment/argocd-applicationset-controller deployment/argocd-redis deployment/argocd-repo-server --timeout=5m
+$k  apply -f - <<EOF
+apiVersion: argoproj.io/v1alpha1
+kind: AppProject
+metadata:
+  name: default
+  namespace: $namespace
+spec:
+  sourceRepos:
+    - '*'
+  destinations:
+    - namespace: '*'
+      server: '*'
+  clusterResourceWhitelist:
+    - group: '*'
+      kind: '*'
+EOF
