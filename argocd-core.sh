3e4e027fc536803230da81e8bf8a22c275b59626
elapse-d <36126914+elapse-d@users.noreply.github.com>
2026-02-07 11:45:58 +0100

    Fix URL in comment for argocd-core.sh

--- argocd-core.sh
+++ argocd-core.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-#curl -sSL https://raw.githubusercontent.com/k8s-cd/common/refs/heads/main/argocd-core.sh | bash
+#curl -sSL https://raw.githubusercontent.com/k8s-cd/common/main/argocd-core.sh | bash
 set -euo pipefail
 use_sudo=true 
 if $use_sudo ; then
@@ -8,4 +8,5 @@ fi
 cluster_name="${1:-${KIND_CLUSTER:-kind-nginx}}"
 kubeconfig="--kubeconfig $HOME/.kube/$cluster_name"
 
+kubectl $kubeconfig create ns argocd-core
 kubectl $kubeconfig -n argocd-core create -f https://raw.githubusercontent.com/argoproj/argo-cd/refs/heads/master/manifests/core-install.yaml
