e38855e958960d9e2c82820b9443a7265d3dc31c
elapse-d <36126914+elapse-d@users.noreply.github.com>
2026-02-07 11:42:30 +0100

    Rearrange variable assignments in argocd-core.sh

--- argocd-core.sh
+++ argocd-core.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 #curl -sSL https://raw.githubusercontent.com/k8s-cd/common/refs/heads/main/argocd-core.sh | bash
 set -euo pipefail
-cluster_name="${1:-${KIND_CLUSTER:-kind-nginx}}"
-use_sudo=true #var_use_sudo
-kubeconfig="--kubeconfig $HOME/.kube/$cluster_name"
-
+use_sudo=true 
 if $use_sudo ; then
   sudo -i
 fi
+cluster_name="${1:-${KIND_CLUSTER:-kind-nginx}}"
+
+kubeconfig="--kubeconfig $HOME/.kube/$cluster_name"
+
+
 kubectl $kubeconfig -n argocd-core create -f https://raw.githubusercontent.com/argoproj/argo-cd/refs/heads/master/manifests/core-install.yaml
