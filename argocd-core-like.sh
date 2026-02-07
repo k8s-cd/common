8a7893af3da218e6c1d09304fa6f88544e9d44bc
elapse-d <36126914+elapse-d@users.noreply.github.com>
2026-02-07 15:33:49 +0100

    Change helm install to helm upgrade --install

--- argocd-core-like.sh
+++ argocd-core-like.sh
@@ -11,14 +11,13 @@ if $use_sudo ; then
   sudo -i
 fi
 cluster_name="${1:-${KIND_CLUSTER:-kind-nginx}}"
-kubeconfig="--kubeconfig $HOME/.kube/$cluster_name"
 export KUBECONFIG="$HOME/.kube/$cluster_name"
 k="kubectl"
 ns=argocd-core
 
 $k create ns $ns
 helm repo add argo https://argoproj.github.io/argo-helm
-helm  install $ns  argo/argo-cd --namespace $ns --set notifications.enabled=false --set dex.enabled=false --set redis.enabled=true --set server.replicas=1 --set configs.cm.admin.enabled=false --set applicationSet.replicas=0 --create-namespace
+helm  upgrade --install $ns  argo/argo-cd --namespace $ns --set notifications.enabled=false --set dex.enabled=false --set redis.enabled=true --set server.replicas=1 --set configs.cm.admin.enabled=false --set applicationSet.replicas=0 --create-namespace
 
 k="$k -n $ns "
 $k wait --for=condition=Available deployment/argocd-applicationset-controller deployment/argocd-redis deployment/argocd-repo-server --timeout=5m
