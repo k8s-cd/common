201ce1ca51ff98590746ff7dee5ee53485b8400d
elapse-d <36126914+elapse-d@users.noreply.github.com>
2026-02-07 15:28:33 +0100

    Refactor kubeconfig setup and kubectl command
    Updated kubeconfig handling and simplified kubectl command.

--- argocd-core-like.sh
+++ argocd-core-like.sh
@@ -12,12 +12,13 @@ if $use_sudo ; then
 fi
 cluster_name="${1:-${KIND_CLUSTER:-kind-nginx}}"
 kubeconfig="--kubeconfig $HOME/.kube/$cluster_name"
-k="kubectl --kubeconfig $kubeconfig"
+export KUBECONFIG="$HOME/.kube/$cluster_name"
+k="kubectl"
 ns=argocd-core
 
 $k create ns $ns
 helm repo add argo https://argoproj.github.io/argo-helm
-helm  install $ns  argo/argo-cd $kubeconfig --namespace $ns --set notifications.enabled=false --set dex.enabled=false --set redis.enabled=true --set server.replicas=1 --set configs.cm.admin.enabled=false --set applicationSet.replicas=0 --create-namespace
+helm  install $ns  argo/argo-cd --namespace $ns --set notifications.enabled=false --set dex.enabled=false --set redis.enabled=true --set server.replicas=1 --set configs.cm.admin.enabled=false --set applicationSet.replicas=0 --create-namespace
 
 k="$k -n $ns "
 $k wait --for=condition=Available deployment/argocd-applicationset-controller deployment/argocd-redis deployment/argocd-repo-server --timeout=5m
