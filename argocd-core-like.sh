c4ddaf9fc1041f8139b03756c886934949364606
elapse-d <36126914+elapse-d@users.noreply.github.com>
2026-02-07 15:39:05 +0100

    Fix helm release name in argocd-core-like.sh

--- argocd-core-like.sh
+++ argocd-core-like.sh
@@ -17,7 +17,7 @@ ns=argocd-core
 
 $k create ns $ns
 helm repo add argo https://argoproj.github.io/argo-helm
-helm  upgrade --install $ns  argo/argo-cd --namespace $ns --set notifications.enabled=false --set dex.enabled=false --set redis.enabled=true --set server.replicas=1 --set configs.cm.admin.enabled=false --set applicationSet.replicas=0 --create-namespace
+helm  upgrade --install argocd  argo/argo-cd --namespace $ns --set notifications.enabled=false --set dex.enabled=false --set redis.enabled=true --set server.replicas=1 --set configs.cm.admin.enabled=false --set applicationSet.replicas=0 --create-namespace
 
 k="$k -n $ns "
 $k wait --for=condition=Available deployment/argocd-applicationset-controller deployment/argocd-redis deployment/argocd-repo-server --timeout=5m
